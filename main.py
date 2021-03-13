920ffa402fef507a4bc20b70f92402281334c4f5
kadirovgm <kadirovgm@gmail.com>
2021-03-13 12:32:54 +0300

    fgfg

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import math
-# # входные данные
-# def quick_power_alg():
-#     x, d, n = 0
+# # # входные данные
+# def quick_power_alg(x,d,n):
+#
 #     # x^d(mod n)
 #     y = 1
 #     while d > 0:
 #         if d % 2 == 1:
-#             y = (y*x) mod n
-#         elif d == d // 2 or x = (x*x) mod n:
+#             y = (y*x) % n
+#         elif d == d // 2:
+#             x = (x * x) % n
 #             break
-#     return y
+#     print(y)
+#
+# quick_power_alg(2, 8, 256)
 
+
+# def fastExp(b, n):
+# #     def even(n):#проверка четности
+# #         if n % 2 == 0:
+# #             return 1
+# #         return 0
+# #     if n == 0:
+# #         return 1
+# #     if even(n):
+# #         return fastExp(b, n/2)**2
+# #     return b*fastExp(b, n-1)
+# #
+# # print(fastExp(2,8))
 
 # # Алгоритм Евклида
 #
@@ -44,28 +60,28 @@ import math
 #     print(a)
 # Evklid_alg_kniga(30,18)
 
-# Расширенный алгоритм Евклида
-# найти d=НОД(m,n) и найти s, t такие, что d = s*m + t*n
-def Evklid_alg_extended(m,n):
-    a = m
-    b = n
-    u1 = 1
-    u2 = 0
-    v1 = 0
-    v2 = 1
-    while b != 0:
-        q = a // b
-        r = a % b
-        a = b
-        b = r
-        r = u2
-        u2 = u1 - q*u2
-        u1 = r
-        r = v2
-        v2 = v1 - q*v2
-        v1 = r
-    d = a
-    s = u1
-    t = v1
-    print(d,s,t)
-Evklid_alg_extended(30,18)
+# # Расширенный алгоритм Евклида
+# # найти d=НОД(m,n) и найти s, t такие, что d = s*m + t*n
+# def Evklid_alg_extended(m,n):
+#     a = m
+#     b = n
+#     u1 = 1
+#     u2 = 0
+#     v1 = 0
+#     v2 = 1
+#     while b != 0:
+#         q = a // b
+#         r = a % b
+#         a = b
+#         b = r
+#         r = u2
+#         u2 = u1 - q*u2
+#         u1 = r
+#         r = v2
+#         v2 = v1 - q*v2
+#         v1 = r
+#     d = a
+#     s = u1
+#     t = v1
+#     print(d,s,t)
+# Evklid_alg_extended(30,18)
